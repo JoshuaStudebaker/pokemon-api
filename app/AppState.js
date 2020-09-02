1ddc20629e655c134af351e313a8eb0e0b57a4b9
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-02 14:04:03 -0600

    basic appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,21 +1,25 @@
-import Value from "./Models/Value.js"
-import { EventEmitter } from "./Utils/EventEmitter.js"
-import { isValidProp } from "./Utils/isValidProp.js"
+import Pokemon from "./Models/Pokemon.js";
+import { EventEmitter } from "./Utils/EventEmitter.js";
+import { isValidProp } from "./Utils/isValidProp.js";
 
 class AppState extends EventEmitter {
-  /** @type {Value[]} */
-  values = []
+  /** @type {Pokemon[]} */
+  apiPokemons = [];
+
+  userPokemons = [];
+
+  activePokemons = null;
 }
 
 export const ProxyState = new Proxy(new AppState(), {
   get(target, prop) {
-    isValidProp(target, prop)
-    return target[prop]
+    isValidProp(target, prop);
+    return target[prop];
   },
   set(target, prop, value) {
-    isValidProp(target, prop)
-    target[prop] = value
-    target.emit(prop, value)
-    return true
-  }
-})
+    isValidProp(target, prop);
+    target[prop] = value;
+    target.emit(prop, value);
+    return true;
+  },
+});
